14ec166fc820c2d459d7900c8527b9c091791f1e
Daniel Bosen <daniel@bosen.eu>
2016-02-18 12:24:04 +0100

    display video

--- src/Plugin/Field/FieldFormatter/NexxVideoPlayer.php
+++ src/Plugin/Field/FieldFormatter/NexxVideoPlayer.php
@@ -7,8 +7,13 @@
 
 namespace Drupal\nexx_integration\Plugin\Field\FieldFormatter;
 
+use Drupal\Component\Utility\Crypt;
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Plugin implementation of the 'nexx_video_player' formatter.
@@ -22,14 +27,77 @@ use Drupal\Core\Field\FieldItemListInterface;
  *   }
  * )
  */
-class NexxVideoPlayer extends FormatterBase {
+class NexxVideoPlayer extends FormatterBase implements ContainerFactoryPluginInterface{
+
+  /**
+   * The The nexx configuration object
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  /**
+   * Constructs a new DateTimeDefaultFormatter.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The formatter settings.
+   * @param string $label
+   *   The formatter label display setting.
+   * @param string $view_mode
+   *   The view mode.
+   * @param array $third_party_settings
+   *   Third party settings.
+   * @param \Drupal\Core\Config\ImmutableConfig $config
+   *   The nexx configuration.
+   */
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, ImmutableConfig $config) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
+
+    $this->config = $config;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['label'],
+      $configuration['view_mode'],
+      $configuration['third_party_settings'],
+      $container->get('config.factory')->get('nexx_integration.settings')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function viewElements(FieldItemListInterface $items, $langcode = NULL) {
-    // Does not actually output anything.
-    return array();
-  }
+    $elements = [];
 
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#theme' => 'nexx_player',
+        '#omnia_id' => $this->config->get('omnia_id'),
+        '#video_id' => $item->item_id,
+        '#container_id' => 'player--' . Crypt::randomBytesBase64(8),
+        /*
+        '#cache' => [
+          'tags' => $user->getCacheTags(),
+        ],
+        */
+      ];
+    }
+
+    return $elements;
+  }
 }
